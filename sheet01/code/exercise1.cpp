94b617ae70cb3975e8535bb5c1eeccb204ab464e
Nico Guth <nico.guth@tu-dortmund.de>
2021-04-19 15:26:42 +0200

    sheet01 exercise1 almost finished

--- sheet01/code/exercise1.cpp
+++ sheet01/code/exercise1.cpp
@@ -13,13 +13,11 @@ int main()
     A << a1,a2,a3;
 
     // b)
-    Eigen::Vector3d b = {2,0,2};
-
     Eigen::PartialPivLU<Eigen::Matrix3d> lu = A.lu();
     Eigen::Matrix3d P = lu.permutationP();
     Eigen::Matrix3d LU = lu.matrixLU();
 
-    
+    Eigen::Vector3d b = {2,0,2};
     Eigen::Vector3d x = lu.solve(b);
     
     cout << "A:" << endl << A << endl;
@@ -28,5 +26,22 @@ int main()
     cout << "x:" << endl << x << endl;
 
     // c)
+    Eigen::Vector3d c = {1,2.*sqrt(3.),3};
+    Eigen::Vector3d y = lu.solve(c);
+
+    cout << "y:" << endl << y << endl;
+
+    // d)
+    Eigen::Matrix3d A2;
+    A2 << a3,a2,a1;
+
+    Eigen::PartialPivLU<Eigen::Matrix3d> lu2 = A2.lu();
+    Eigen::Matrix3d P2 = lu2.permutationP();
+    Eigen::Matrix3d LU2 = lu2.matrixLU();
+
+    cout << "A2:" << endl << A2 << endl;
+    cout << "P2:" << endl << P2 << endl;
+    cout << "LU2:" << endl << LU2 << endl;
+
     return 0;
 }
