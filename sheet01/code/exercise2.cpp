fe2e230a0ff0fb6f6dee2fe8135f01597a781ac7
Nico Guth <nico.guth@tu-dortmund.de>
2021-04-20 14:31:44 +0200

    sheet01 exercise2 code finished

--- sheet01/code/exercise2.cpp
+++ sheet01/code/exercise2.cpp
@@ -9,7 +9,7 @@ using namespace std;
 int main()
 {
     // read in the data
-    fstream file("exercise2.csv", ios::in);
+    ifstream file("exercise2.csv", ios::in);
     Eigen::VectorXd x;
     Eigen::VectorXd y;
     if(file.is_open())
@@ -34,11 +34,38 @@ int main()
         }
     }
     file.close();
-    
+
     cout << "x:" << endl << x << endl;
     cout << "y:" << endl << y << endl;
-
     
+    // c)
+    Eigen::Matrix2d A;
+    Eigen::Vector2d b;
+    A << x.dot(x), x.sum(), 
+        x.sum(), x.size();
+    b << x.dot(y), y.sum();
+
+    Eigen::PartialPivLU<Eigen::Matrix2d> lu = A.lu();
+
+    Eigen::Matrix2d P = lu.permutationP();
+    Eigen::Matrix2d LU = lu.matrixLU();
+
+    Eigen::Vector2d mn = lu.solve(b);
+
+    cout << "A:" << endl << A << endl;
+    cout << "P:" << endl << P << endl;
+    cout << "LU:" << endl << LU << endl;
+    cout << "b:" << endl << b << endl;
+    cout << "mn:" << endl << mn << endl;
+
+    // save m and n for Python
+    ofstream file2("build/exercise2_mn.csv");
+    if(file2.is_open())
+    {
+        file2 << "m,n" << endl;
+        file2 << mn(0) << "," << mn(1) << endl;
+    }
+    file2.close();
 
     return 0;
 }
---
+++ sheet01/code/exercise2.py
@@ -0,0 +1,26 @@
+import numpy as np
+import matplotlib.pyplot as plt 
+from scipy.optimize import curve_fit
+
+# read in the data
+x,y = np.genfromtxt("exercise2.csv",delimiter=',',unpack=True,skip_header=1)
+m,n = np.genfromtxt("build/exercise2_mn.csv",delimiter=',',unpack=True,skip_header=1)
+
+# funktion for the linear regression
+f = lambda x,m,n : m*x + n
+
+# python curve fit for comparison
+params,pcov = curve_fit(f,x,y)
+
+print(f"C++ linear regression: \t m = {m}; n = {n}")
+print(f"Python curve_fit: \t m = {params[0]:.6f}; n = {params[1]:.5f}")
+
+plt.xlabel("x")
+plt.ylabel("y")
+
+plt.plot(x,f(x,m,n),'b-', label="C++ linear regression")
+plt.plot(x,y,'ko', label="data points")
+
+plt.tight_layout()
+plt.legend()
+plt.savefig("build/plot_exercise2.pdf")
