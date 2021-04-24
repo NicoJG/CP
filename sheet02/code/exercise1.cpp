debd3a63d4cb2b9d2ef691f7a93cd026ea71d2f4
Nico Guth <nico.guth@tu-dortmund.de>
2021-04-24 18:16:51 +0200

    sheet02 excercise1 code finished

--- sheet02/code/exercise1.cpp
+++ sheet02/code/exercise1.cpp
@@ -1,10 +1,40 @@
 #include <iostream>
 #include <eigen3/Eigen/Dense>
+#include "service.cpp"
 
 using namespace std;
 
 int main()
 {
-    
+    // a) 
+    // read Image data
+    Eigen::MatrixXd img;
+    if(!loadData(img,"Image",512,512))
+    {
+       cerr << "Failed to load the File." << endl;
+       return 0;
+    }
+    // perform SVD
+    Eigen::BDCSVD<Eigen::MatrixXd> svd = img.bdcSvd(Eigen::ComputeFullU | Eigen::ComputeFullV);
+    Eigen::MatrixXd U = svd.matrixU();
+    Eigen::MatrixXd V = svd.matrixV();
+    Eigen::VectorXd W = svd.singularValues(); // already sorted decending
+
+    // b) 
+    // rank k=(10,20,50) approximation
+    vector<int> ks = {10,20,50};
+    cout << "Image Compression:" << endl;
+    for(int i = 0; i<ks.size(); i++)
+    {
+        int k = ks[i];
+        cout << "k=" << k << endl;
+        Eigen::MatrixXd A_k = Eigen::MatrixXd::Zero(img.cols(),img.rows());
+        for(int j = 0; j<k; j++)
+        {
+            A_k = A_k + W(j) * U.col(j) * V.col(j).transpose();
+        }
+        storeData(A_k,"build/Image_"+to_string(k));
+    }
+
     return 0;
 }
--- sheet02/code/exercise1.py
+++ sheet02/code/exercise1.py
@@ -1,2 +1,35 @@
+import os
 import numpy as np
 import matplotlib.pyplot as plt 
+
+# prepare the subplots
+rows = 2
+cols = 2
+plt.subplots(rows,cols)
+
+# Plot the Original Image
+img = np.fromfile("Image",np.uint8).reshape(512,512)
+plt.subplot(rows,cols,1)
+plt.title("Original Image")
+plt.imshow(img,cmap="gray", vmin=0, vmax=255)
+
+# get available k-compressed images
+ks = []
+for file in os.listdir("build/"):
+    if file.startswith("Image_"):
+        ks.append(int(file.split('_')[1]))
+ks = sorted(ks)
+
+# Plot all k-compressed images
+i = 2
+for k in ks:
+    img = np.fromfile(f'build/Image_{k}',np.uint8).reshape(512,512)
+    plt.subplot(rows,cols,i)
+    plt.title(f"Compressed Image k={k}")
+    plt.imshow(img,cmap="gray", vmin=0, vmax=255)
+    i += 1
+    if i>rows*cols:
+        break
+
+plt.tight_layout()
+plt.show()
