ae30fe32ad92c21666b0368f94d78a8d90c2454b
Nico Guth <nico.guth@tu-dortmund.de>
2021-04-27 14:49:55 +0200

    sheet02 exercise2 python plot

--- sheet02/code/exercise2.py
+++ sheet02/code/exercise2.py
@@ -1,2 +1,17 @@
 import numpy as np
 import matplotlib.pyplot as plt 
+
+data = np.genfromtxt("build/exercise3_timing_data.csv",delimiter=',')
+
+N = data[:,0]
+t = data[:,1:]
+
+for i in range(3):
+    plt.plot(N,t[:,i],'-',label=f"a.{i})")
+
+plt.xlabel("$N$")
+plt.ylabel("$t \:/\: s$")
+
+plt.legend()
+plt.tight_layout()
+plt.savefig("build/plot_exercise3.pdf")
