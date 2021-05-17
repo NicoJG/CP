5aaf8a4320270a33366038c6e0c9a39112537f8f
Jan Jaekel <jan.jaekel@tu-dortmund.de>
2021-05-17 17:48:32 +0200

    improved ex01

--- sheet05/code/exercise1.cpp
+++ sheet05/code/exercise1.cpp
@@ -15,7 +15,7 @@ double I_2(double x)
 }
 double I_3(double x)
 {
-    return sin(x) / x;
+    return sin(1 / x) / x;
 }
 
 double trapezoidal(double(* func)(double), double a, double b, int N)
@@ -44,14 +44,27 @@ double simpson(double(* func)(double), double a, double b, int N)
     }
     return h / 6 * result;
 }
+double midpoint(double( *func)(double), double a, double b, int N)
+{   
+    double h = (b - a) / N;
+    double result = 0;
+
+    for (int i = 1; i < N; i++)
+    {
+        result += func(a + i*h - h/2);
+    }
+
+    return h * result;
+}
+
 int main()
 {    
     
-    std::cout <<"I_1:trap "<< trapezoidal(I_1,1,-1,15) << std::endl;//no idea why b=-1 and a=1 works;N=15 randomly worked...
-    std::cout <<"I_1:simps "<< simpson(I_1,-1,1,15) << std::endl;
-    std::cout <<"I_2:trap "<< trapezoidal(I_2,0,100,10000000) << std::endl;// error should not be to high with x_max=100 because e^(-100) is already 10^-44
-    std::cout <<"I_2:simps "<< simpson(I_2,0,100,10000000) << std::endl;
-    std::cout <<"I_3:trap "<< trapezoidal(I_3,-1e8,1e8,15) << std::endl;// I don't know what values for a and b would be apporpriate
-    std::cout <<"I_3:simps "<< simpson(I_3,-1e8,1e8,12) << std::endl;
+    std::cout <<"I_1:trap "<< trapezoidal(I_1,1,-1,15) << std::endl;
+    
+    std::cout <<"I_2:trap "<< trapezoidal(I_2,0,100,1e7) << std::endl;// error should not be to high with x_max=100 because e^(-100) is already 10^-44
+    
+    std::cout <<"I_3:mid "<< - midpoint(I_3,1,0,1e5) - midpoint(I_3,0,1,1e5)<< std::endl;//midpoint bc its belongs to the OCN and ignores the singularity
+    
     return 0;   
 }
